f2fd848a6b2564e6b873a54f55034b1164d0ddd5
Kiril Vatev <vatev.1@gmail.com>
2017-03-20 20:46:28 -0400

    fixing config script lint issues

--- config/paths.js
+++ config/paths.js
@@ -1,9 +1,10 @@
-var path = require('path');
-var fs = require('fs');
+const path = require('path');
+const fs = require('fs');
 
 // Make sure any symlinks in the project folder are resolved:
 // https://github.com/facebookincubator/create-react-app/issues/637
-var appDirectory = fs.realpathSync(process.cwd());
+const appDirectory = fs.realpathSync(process.cwd());
+
 function resolveApp(relativePath) {
   return path.resolve(appDirectory, relativePath);
 }
@@ -18,7 +19,7 @@ function resolveApp(relativePath) {
 // We will export `nodePaths` as an array of absolute paths.
 // It will then be used by Webpack configs.
 
-var nodePaths = (process.env.NODE_PATH || '')
+const nodePaths = (process.env.NODE_PATH || '')
   .split(process.platform === 'win32' ? ';' : ':')
   .filter(Boolean)
   .map(resolveApp);
@@ -35,5 +36,5 @@ module.exports = {
   testsSetup: resolveApp('src/setupTests.js'),
   appNodeModules: resolveApp('node_modules'),
   ownNodeModules: resolveApp('node_modules'),
-  nodePaths: nodePaths
+  nodePaths
 };
