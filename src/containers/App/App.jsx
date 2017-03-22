51cb1abc4e07a32c63dd1242bb57d5fff292f7b4
Kiril Vatev <vatev.1@gmail.com>
2017-03-21 23:01:22 -0400

    more chords displayed on the app

--- src/containers/App/App.jsx
+++ src/containers/App/App.jsx
@@ -5,12 +5,18 @@ import './App.scss';
 export default function App() {
   return (
     <div>
+      <UkuleleChord name="A" />
       <UkuleleChord name="Am" />
+      <UkuleleChord name="A#" />
+      <UkuleleChord name="Bb" />
+      <UkuleleChord name="B" />
       <UkuleleChord name="Bm" />
       <UkuleleChord name="C" />
       <UkuleleChord name="D" />
       <UkuleleChord name="Em" />
       <UkuleleChord name="G" />
+      <UkuleleChord name="Gm" />
+      <UkuleleChord name="G7" />
       <UkuleleChord name="F" />
     </div>
   );
