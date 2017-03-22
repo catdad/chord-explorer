be733f9097e4c5d60402eb653d6785bcca493c01
Kiril Vatev <vatev.1@gmail.com>
2017-03-21 20:28:28 -0400

    fixing name of component

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,3 @@
 export { default as Button } from './Button/Button';
 export { default as Icon } from './Icon/Icon';
-export { default as PlayLevel } from './UkuleleChord/UkuleleChord';
+export { default as UkuleleChord } from './UkuleleChord/UkuleleChord';
