a3ee16a4ac22b9376a2e5b4347b2c866d29fce4b
Kiril Vatev <vatev.1@gmail.com>
2017-03-23 22:41:48 -0400

    more chords

--- src/containers/App/App.jsx
+++ src/containers/App/App.jsx
@@ -7,7 +7,7 @@ function unique(arr) {
 }
 
 export default function App() {
-  const str = 'A Am A# Am7 Bb B Bm C D Em G Gm G7 F Q';
+  const str = 'A Am A# Am7 Bb B Bm C D Em E7 G Gm G7 F Q';
 
   const chords = unique(str.split(' '))
     .map((name) => <UkuleleChord key={ name } name={ name } />);
