878ee85de1c98440bada4bd71a4fcfa6167bcc36
Kiril Vatev <vatev.1@gmail.com>
2017-03-25 13:27:03 -0400

    more chords displayed in the default app

--- src/containers/App/App.jsx
+++ src/containers/App/App.jsx
@@ -7,7 +7,7 @@ function unique(arr) {
 }
 
 export default function App() {
-  const str = 'A Am A# Am7 Bb B Bm C D Em E7 G Gm G7 F Q';
+  const str = 'A Am A# Am7 Bb B Bm Bm7 C D Em E7 G Gm G7 F Fmaj7 Q open';
 
   const chords = unique(str.split(' '))
     .map((name) => <UkuleleChord key={ name } name={ name } />);
