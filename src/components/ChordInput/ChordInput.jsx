202ec1c1bafc6b38ef426e10100a5b6b1096a818
Kiril Vatev <vatev.1@gmail.com>
2017-03-29 19:50:05 -0400

    hooking up an action for a change on the chord input

--- src/components/ChordInput/ChordInput.jsx
+++ src/components/ChordInput/ChordInput.jsx
@@ -2,26 +2,25 @@ import React, { PureComponent, PropTypes } from 'react';
 
 export default class ChordInput extends PureComponent {
   static propTypes = {
-    onSubmit: PropTypes.func.isRequired
+    onChange: PropTypes.func.isRequired
   };
 
   constructor(props) {
     super(props);
 
-    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
   }
 
-  handleSubmit(ev) {
-    global.console.log(ev);
-    const { onSubmit } = this.props;
+  handleChange(ev) {
+    const { onChange } = this.props;
 
-    onSubmit(ev.target.value);
+    onChange(ev.target.value);
   }
 
   render() {
     return (
       <div>
-        <input onChange={ this.handleSumit } />
+        <input onChange={ this.handleChange } />
       </div>
     );
   }
--- src/containers/App/App.jsx
+++ src/containers/App/App.jsx
@@ -1,32 +1,58 @@
-import React from 'react';
+import React, { PropTypes, PureComponent } from 'react';
+import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import { UkuleleChord, ChordInput } from '../../components';
+import * as actionCreators from '../../actions';
 import './App.scss';
 
 function unique(arr) {
   return Array.from(new Set(arr));
 }
 
-function App(/* { chords } */) {
-  const str = 'A Am A# Am7 Bb B Bm Bm7 C D Em E7 G Gm G7 F Fmaj7 Q open';
+class App extends PureComponent {
+  static propTypes = {
+    chords: PropTypes.string.isRequired,
+    actions: PropTypes.objectOf(PropTypes.func).isRequired
+  };
 
-  const chordElems = unique(str.split(' '))
-    .map((name) => <UkuleleChord key={ name } name={ name } />);
+  constructor(props) {
+    super(props);
 
-  function onSubmit(val) {
-    global.console.log(val);
+    this.handleChordChange = this.handleChordChange.bind(this);
   }
 
-  return (
-    <div>
-      <ChordInput onSubmit={ onSubmit } />
-      <div>{ chordElems }</div>
-    </div>
-  );
+  handleChordChange(value) {
+    global.console.log(value);
+
+    this.props.actions.setChords(value);
+  }
+
+  render() {
+    const str = 'A Am A# Am7 Bb B Bm Bm7 C D Em E7 G Gm G7 F Fmaj7 Q open';
+
+    const chordElems = unique(str.split(' '))
+      .map((name) => <UkuleleChord key={ name } name={ name } />);
+
+    global.console.log(this.props.chords);
+
+    return (
+      <div>
+        <ChordInput onChange={ this.handleChordChange } />
+        <div>{ chordElems }</div>
+      </div>
+    );
+  }
 }
 
 const mapStateToProps = ({ chords }) => ({
   chords
 });
 
-export default connect(mapStateToProps)(App);
+const mapDispatchToProps = (dispatch) => ({
+  actions: bindActionCreators(actionCreators, dispatch)
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(App);
