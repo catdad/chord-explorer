016af3e90567b3c8746f7814fea348a6e9fb8539
Kiril Vatev <vatev.1@gmail.com>
2017-03-29 20:21:52 -0400

    cleanup

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -26,10 +26,6 @@ const chords = createReducer({
   }
 });
 
-//  function chords(state = DEFAULT_CHORDS, action) {
-//    return action.value || state;
-//  }
-
 const rootReducer = combineReducers({
   chords
 });
