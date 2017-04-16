84525ae2fc9ce1f885c0645f4da1415a9fe397ec
Kiril Vatev <vatev.1@gmail.com>
2017-04-16 14:41:02 -0400

    using prop-types modules instead of PropTypes from React

--- src/components/Icon/Icon.jsx
+++ src/components/Icon/Icon.jsx
@@ -1,4 +1,5 @@
-import React, { PropTypes, PureComponent } from 'react';
+import React, { PureComponent } from 'react';
+import PropTypes from 'prop-types';
 import * as icons from '../../constants/icons';
 import './Icon.scss';
 
