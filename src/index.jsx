0b3a966dbc49ded3243e83c756e93a21dc8cb4f3
Kiril Vatev <vatev.1@gmail.com>
2017-04-30 12:35:08 -0400

    adding middleware to manage url history

--- src/index.jsx
+++ src/index.jsx
@@ -1,17 +1,43 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import { Router, Route } from 'react-router';
 import { createHistory, useBasename } from 'history';
 import { App } from './containers';
 import reducer from './reducers';
+import * as types from './constants/actionTypes';
 
-const store = createStore(reducer);
+/* eslint-disable */
 
 const browserHistory = useBasename(createHistory)({
   basename: process.env.PUBLIC_URL // eslint-disable-line no-process-env
 });
+
+const middleware = (store) => (next) => (action) => {
+  let before = store.getState();
+  console.log(action);
+
+  let result = next(action);
+  let after = store.getState();
+
+  console.log('before', before);
+  console.log('after', after);
+  console.log('result', result);
+
+  if (action !== types.SET_CHORDS) {
+    return result;
+  }
+
+  const location = browserHistory.location;
+  const newLocation = `/ukulele/${after.chords.sanitized}`;
+
+  browserHistory.replace(newLocation, { chordstring: after.chords.sanitized });
+
+  return result;
+};
+
+const store = createStore(reducer, applyMiddleware(middleware));
 
 render(
   <Provider store={ store }>
