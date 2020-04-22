a4acef89e6c727ce7a8d2667aa01bd0be1917488
Kiril Vatev <vatev.1@gmail.com>
2020-04-22 17:57:51 -0400

    getting rid of redux, router, converting App a functional component

--- src/containers/App/App.jsx
+++ src/containers/App/App.jsx
@@ -1,84 +1,68 @@
-import React, { PureComponent } from 'react';
-import PropTypes from 'prop-types';
-import { bindActionCreators } from 'redux';
-import { connect } from 'react-redux';
+import React, { useCallback, useState } from 'react';
+import { createHashHistory } from 'history';
+
+import { inputToData, urlToData, urlParts } from '../../actions';
+
 import { UkuleleChord, ChordInput, EmptyChordlist } from '../../components';
-import * as actionCreators from '../../actions';
 import './App.scss';
 
-class App extends PureComponent {
-  constructor(props) {
-    super(props);
+// eslint-disable-next-line no-console
+const inspect = (...args) => console.log(...args);
 
-    this.handleChordChange = this.handleChordChange.bind(this);
-  }
+const history = createHashHistory({
+  // no basename is necessary with hash history
+  basename: '/',
+  hashType: 'slash'
+});
 
-  componentDidMount() {
-    const { chordstring } = this.props.params;
+function App() {
+  const [chordState, setChordState] = useState(urlToData(urlParts().chords));
+  // const [instrument, setInstrument] = useState(urlParts().instrument);
 
-    if (chordstring) {
-      this.props.actions.setChordstring(chordstring);
+  const { array: chords, input } = chordState;
+
+  inspect('RENDER APP WITH CHORD STATE', chordState);
+
+  const handleChordChange = useCallback((val) => {
+    const data = inputToData(val);
+
+    if (chordState.sanitized !== data.sanitized) {
+      inspect('chord change', val, data);
+
+      // we'll replace the state when chords are updated, to avoid having
+      // a brand new history entry every time the user types a character
+      history.replace(`/ukulele/${data.sanitized}`, { chords: data });
     }
-  }
 
-  handleChordChange(value) {
-    this.props.actions.setChords(value);
-  }
+    setChordState(data);
+  });
 
-  render() {
-    const chordElems = this.props.chords.array
-      .map((name) => <UkuleleChord key={ name } name={ name } />);
+  const chordElems = chords.map((name) => (<UkuleleChord key={ name } name={ name } />));
 
-    return (
-      <div className="app-body">
-        <div className="app-header-wrap">
-          <div className="header-input">
-            <ChordInput
-              onChange={ this.handleChordChange }
-              value={ this.props.chords.value }
-            />
-          </div>
-        </div>
-        <div className="app-body-wrap">{
-          chordElems.length ?
-            <div className="chord-body">{ chordElems }</div> :
-            <EmptyChordlist />
-        }</div>
-        <div className="app-footer-wrap">
-          <span>{ 'Made with 💜 and 🍸 by ' }</span>
-          <a href="https://github.com/catdad">{ '@catdad' }</a>
-          <span>{ '.' }</span>
+  inspect('chords:', chordElems.length);
+
+  return (
+    <div className="app-body">
+      <div className="app-header-wrap">
+        <div className="header-input">
+          <ChordInput
+            onChange={ handleChordChange }
+            value={ input }
+          />
         </div>
       </div>
-    );
-  }
+      <div className="app-body-wrap">{
+        chordElems.length ?
+          <div className="chord-body">{ chordElems }</div> :
+          <EmptyChordlist />
+      }</div>
+      <div className="app-footer-wrap">
+        <span>{ 'Made with 💜 and 🍸 by ' }</span>
+        <a href="https://github.com/catdad">{ '@catdad' }</a>
+        <span>{ '.' }</span>
+      </div>
+    </div>
+  );
 }
 
-App.propTypes = {
-  chords: PropTypes.shape({
-    value: PropTypes.string.isRequired,
-    array: PropTypes.arrayOf(PropTypes.string).isRequired,
-    sanitized: PropTypes.string.isRequired
-  }).isRequired,
-  actions: PropTypes.objectOf(PropTypes.func).isRequired,
-  params: PropTypes.shape({
-    chordstring: PropTypes.string
-  })
-};
-
-App.defaultProps = {
-  params: {}
-};
-
-const mapStateToProps = ({ chords }) => ({
-  chords
-});
-
-const mapDispatchToProps = (dispatch) => ({
-  actions: bindActionCreators(actionCreators, dispatch)
-});
-
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(App);
+export default App;
--- src/index.jsx
+++ src/index.jsx
@@ -1,47 +1,9 @@
 import React from 'react';
 import { render } from 'react-dom';
 
-import { createStore, applyMiddleware } from 'redux';
-import { Provider } from 'react-redux';
-
-import { Router, Route } from 'react-router';
-import { createHashHistory } from 'history';
 import { App } from './containers';
-import reducer from './reducers';
-import * as types from './constants/actionTypes';
-
-const browserHistory = createHashHistory({
-  // no basename is necessary with hash history
-  basename: '/', // process.env.PUBLIC_URL, // eslint-disable-line no-process-env
-  hashType: 'slash'
-});
-
-const middleware = (store) => (nextState) => (action) => {
-  const result = nextState(action);
-  const after = store.getState();
-
-  if (action.type !== types.SET_CHORDS) {
-    return result;
-  }
-
-  const url = `/ukulele/${after.chords.sanitized}`;
-
-  // we'll replace the state when chords are updated, to avoid having
-  // a brand new history entry every time the user types a character
-  browserHistory.replace(url, { chordstring: after.chords.sanitized });
-
-  return result;
-};
-
-const store = createStore(reducer, applyMiddleware(middleware));
 
 render(
-  <Provider store={ store }>
-    <Router history={ browserHistory }>
-      <Route path="/" component={ App } />
-      <Route path="/ukulele" component={ App } />
-      <Route path="/ukulele/:chordstring" component={ App } />
-    </Router>
-  </Provider>,
+  <App />,
   document.getElementById('root')
 );
