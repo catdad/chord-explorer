26e7186ece28368b016b9619294394397d51f315
Kiril Vatev <vatev.1@gmail.com>
2020-04-27 18:31:25 -0400

    implementing mutes

--- src/components/Chord/Fingers.jsx
+++ src/components/Chord/Fingers.jsx
@@ -2,8 +2,23 @@ import React from 'react';
 
 const FRETSIZE = 99 / 4;
 const RADIUS = 5;
+const LINE_COMP = 0.5;
 
 const wrap = (elems) => (<g className="fingers">{ elems }</g>);
+
+const position = (fret, i, interval, start) => {
+  const x = (interval * i) + start + LINE_COMP;
+  const y = ((FRETSIZE * fret) - (FRETSIZE / 2)) + LINE_COMP;
+
+  const r = Math.ceil((RADIUS + 2) / 2);
+
+  const x1 = x - r;
+  const x2 = x + r;
+  const y1 = y - r;
+  const y2 = y + r;
+
+  return { x, y, x1, x2, y1, y2 }; // eslint-disable-line object-property-newline
+};
 
 export default function Fingers({ fingers = null } = {}) {
   if (fingers === null || !fingers.length) {
@@ -25,13 +40,27 @@ export default function Fingers({ fingers = null } = {}) {
       return null;
     }
 
+    if (isNaN(fret)) {
+      const { x1, x2, y1, y2 } = position(1, i, interval, start);
+
+      return (
+        <path
+          key={ `${i + i}` }
+          className="mute"
+          d={ `M ${x1} ${y1} L ${x2} ${y2} M ${x1} ${y2} L ${x2} ${y1} Z` }
+        />
+      );
+    }
+
+    const { x, y } = position(1, i, interval, start);
+
     return (
       <circle
         key={ `${i + i}` }
         className="finger"
         r={ RADIUS }
-        cy={ ((FRETSIZE * fret) - (FRETSIZE / 2)) }
-        cx={ (interval * i) + start }
+        cy={ y }
+        cx={ x }
       />
     );
   }).filter((i) => i));
