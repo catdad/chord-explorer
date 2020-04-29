548097d844e437a47afa9d865ec28ac6ed4188a0
Kiril Vatev <vatev.1@gmail.com>
2020-04-28 20:03:26 -0400

    generating a mui theme based on the css colors

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { ThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 
 import App from './components/App/App';
 import './global.css';
 
+const style = getComputedStyle(document.documentElement);
+
+const theme = createMuiTheme({
+  palette: {
+    primary: { main: style.getPropertyValue('--primary').trim() },
+    secondary: { main: style.getPropertyValue('--secondary').trim() }
+  }
+});
+
 render(
-  <App />,
+  <ThemeProvider theme={ theme } >
+    <App />
+  </ThemeProvider>,
   document.getElementById('root')
 );
