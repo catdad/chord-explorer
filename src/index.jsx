fa848bde07cb4d06f889e18cc40aac079c46917e
Kiril Vatev <vatev.1@gmail.com>
2020-04-29 18:03:44 -0400

    re-enabling auto dark mode

--- src/index.jsx
+++ src/index.jsx
@@ -7,11 +7,11 @@ import './global.css';
 
 const style = getComputedStyle(document.documentElement);
 const getVar = (name) => style.getPropertyValue(name).trim();
-// const userPrefersDark = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
+const userPrefersDark = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
 
 const theme = createMuiTheme({
   palette: {
-    type: 'light', // userPrefersDark ? 'dark' : 'light',
+    type: userPrefersDark ? 'dark' : 'light',
     primary: { main: getVar('--primary') },
     secondary: { main: getVar('--secondary') },
     text: { primary: getVar('--color-text') },
