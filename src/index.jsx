26f80db27f5a5a5f30c395eda83289e0c7208a2a
Kiril Vatev <vatev.1@gmail.com>
2020-04-28 20:30:10 -0400

    detecting dark mode and changing colors, setting dark theme when user prefers is

--- src/index.jsx
+++ src/index.jsx
@@ -7,12 +7,18 @@ import './global.css';
 
 const style = getComputedStyle(document.documentElement);
 const getVar = (name) => style.getPropertyValue(name).trim();
+const userPrefersDark = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
 
 const theme = createMuiTheme({
   palette: {
+    type: userPrefersDark ? 'dark' : 'light',
     primary: { main: getVar('--primary') },
     secondary: { main: getVar('--secondary') },
-    text: { primary: getVar('--color-text') }
+    text: { primary: getVar('--color-text') },
+    background: {
+      default: getVar('--color-background'),
+      paper: getVar('--color-background')
+    }
   }
 });
 
