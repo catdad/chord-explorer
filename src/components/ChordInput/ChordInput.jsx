4a2dab27391e2a93b7dcdb1948b2f2eef004bfe4
Kiril Vatev <vatev.1@gmail.com>
2020-05-06 17:51:25 -0400

    removing unnecessary component

--- src/components/ChordInput/ChordInput.jsx
+++ src/components/ChordInput/ChordInput.jsx
@@ -1,16 +1,15 @@
 import React from 'react';
-import FancyInput from './FancyField';
 
 import './ChordInput.css';
 import { noop } from '../../utils.js';
 
 export default function ChordInput({ value = '', onChange = noop } = {}) {
   return (
-    <FancyInput
-      className="chord-input"
-      label="Chord list"
-      value={ value }
-      onChange={ onChange }
-    />
+    <div className="chord-input">
+      <div className="floating-input">
+        <input placeholder={ ' ' } onChange={ onChange } value={ value } />
+        <label>{ 'Chord list' }</label>
+      </div>
+    </div>
   );
 }
