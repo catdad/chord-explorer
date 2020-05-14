595917a71d780f83356ba34bb1c84cc18f627cb4
Kiril Vatev <vatev.1@gmail.com>
2020-05-13 20:50:15 -0400

    adding a cache manifest

--- src/sw.js
+++ src/sw.js
@@ -1,5 +1,7 @@
 /* eslint-disable no-console */
 
+self.importScripts('precache-manifest.js');
+
 self.addEventListener('fetch', (event) => {
   console.log('fetch', event.request.method, event.request.url);
   event.respondWith(fetch(event.request));
